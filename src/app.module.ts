1df7101053abddd78571ef2c11e5b711cec8e34e
zdn-tuan-nguyen <tuan.nguyen@zodinet.tech>
2025-09-13 12:12:59 +0700

    feat: add catalog and inventory modules with controllers, services, and DTOs; implement shopping module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { UserModule } from './modules/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './db/data-source';
 import { AuthModule } from './modules/auth/auth.module';
+import { CatalogModule } from './modules/catalog/catalog.module';
+import { ShoppingModule } from './modules/shopping/shopping.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule, CatalogModule, ShoppingModule, InventoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
