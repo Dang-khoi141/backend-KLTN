4c2e8562e774f1f9132c41d8c548eaf78b476250
Fedelya <anhtientran131103@gmail.com>
2025-10-11 00:35:20 +0700

    fix: add endpoint createPayment

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -2,10 +2,16 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentWebhookGuard } from './guards/payment-webhook.guard';
 import type { PayosWebhookBodyPayload } from './dto/payos-webhook-body.payload';
+import type { CreatePaymentDto } from './types/dto';
 
 @Controller('payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Post()
+  async createPayment(@Body() body: CreatePaymentDto): Promise<any> {
+    return this.paymentService.createPayment(body);
+  }
 
   @Post('webhook')
   @UseGuards(PaymentWebhookGuard)
